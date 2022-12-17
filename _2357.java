6a8dd3a08fc27e5b47454c039c02848a655df043
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-12-17 20:36:11 +0530

    Add files via upload

--- _2357.java
+++ _2357.java
@@ -1,18 +1,40 @@
+/*
+2357. Make Array Zero by Subtracting Equal Amounts
+You are given a non-negative integer array nums. In one operation, you must:
+
+Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+Subtract x from every positive element in nums.
+Return the minimum number of operations to make every element in nums equal to 0.
+ */
 package Leetcode;
+import java.util.Arrays;
 
 public class _2357 {
     public static void main(String[] args) {
-        String[] arr = {"feh","w","w","lwd","c","s","vk","zwlv","n","w","sw","qrd","w","w","mqe","w","w","w","gb","w","qy","xs","br","w","rypg","wh","g","w","w","fh","w","w","sccy"};
-        String s = "w";
-        System.out.println(find(arr , s));
+        int[] arr = {1,5,0,3,5};
+        System.out.println(find(arr));
     }
-    static int find(String[] words, String s){
+    static int find(int[] arr){
         int count = 0;
-        for(int i = 0; i < words.length; i++){
-            String s1 = words[i];
-            if(s1.charAt(0) == s.charAt(0)) {
-                count++;
+        Arrays.sort(arr);
+        while(true) {
+            int x = 0;
+            for (int i = count; i < arr.length; i++) {
+                if (arr[i] != 0) {
+                    x = arr[i];
+                    break;
+                }
             }
+            if(x == 0) break;
+            for (int i = arr.length-1; i >= 0; i--) {
+                if(arr[i] != 0) {
+                    arr[i] = arr[i] - x;
+                }
+                else{
+                    break;
+                }
+            }
+            count++;
         }
         return count;
     }
